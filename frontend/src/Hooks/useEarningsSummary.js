b936e34c9d62e37d2ab0f404edfc8e7361eae8d2
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:11:02 +0300

    Update useEarningsSummary.js

--- frontend/src/Hooks/useEarningsSummary.js
+++ frontend/src/Hooks/useEarningsSummary.js
@@ -1,17 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchEarningsSummary } from "../api/earnings";
 
-export function useEarningsSummary() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["earnings-summary"],
-    queryFn: async () => {
-      const res = await fetchEarningsSummary();
-      return res.data;
-    },
-  });
-
-  return {
-    data,
-    loading: isLoading,
-  };
-}
