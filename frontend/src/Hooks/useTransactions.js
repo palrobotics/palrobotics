098cb341aabda1b2ac74a47df6d9c216dffa1303
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:11:55 +0300

    Update useTransactions.js

--- frontend/src/Hooks/useTransactions.js
+++ frontend/src/Hooks/useTransactions.js
@@ -1,15 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTransactions } from "../api/transactions.api";
 
-export function useTransactions(type) {
-  const { data, isLoading, refetch } = useQuery({
-    queryKey: ["transactions", type],
-    queryFn: () => fetchTransactions(type),
-  });
-
-  return {
-    transactions: data || [],
-    loading: isLoading,
-    refetch,
-  };
-}
