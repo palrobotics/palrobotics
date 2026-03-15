52200218d732f0808aebe7a9fb9780aba0b06031
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:11:41 +0300

    Update useTeamCount.js

--- frontend/src/Hooks/useTeamCount.js
+++ frontend/src/Hooks/useTeamCount.js
@@ -1,17 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import api from "../api/api";
 
-export function useTeamCount() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["team-count"],
-    queryFn: async () => {
-      const res = await api.get("/referrals/team_count");
-      return res.data.count;
-    },
-  });
-
-  return {
-    count: data || 0,
-    loading: isLoading,
-  };
-}
