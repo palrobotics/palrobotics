6048c008f2ddf5d9b04603f3a5f2d6e9ed9aadb4
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:10:49 +0300

    Update useEarningsChart.js

--- frontend/src/Hooks/useEarningsChart.js
+++ frontend/src/Hooks/useEarningsChart.js
@@ -1,17 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchEarningsChart } from "../api/earnings";
 
-export function useEarningsChart() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["earnings-chart"],
-    queryFn: async () => {
-      const res = await fetchEarningsChart();
-      return res.data.chartData || [];
-    },
-  });
-
-  return {
-    data: data || [],
-    loading: isLoading,
-  };
-}
