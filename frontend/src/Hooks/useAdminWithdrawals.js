7183b586424d798ac11fd791b8f095a72eb7ad52
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:10:23 +0300

    Update useAdminWithdrawals.js

--- frontend/src/Hooks/useAdminWithdrawals.js
+++ frontend/src/Hooks/useAdminWithdrawals.js
@@ -1,19 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { adminApi } from "../api/adminApi";
 
-export function useAdminWithdrawals() {
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["admin-withdrawals"],
-    queryFn: async () => {
-      const res = await adminApi.getPendingWithdrawals();
-      return res.data.withdrawals;
-    },
-  });
-
-  return {
-    withdrawals: data || [],
-    loading: isLoading,
-    error,
-    refresh: refetch,
-  };
-}
