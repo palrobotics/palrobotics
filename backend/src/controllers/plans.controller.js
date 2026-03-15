4c2901cb0db15fdb59564d26e2e6a43ec52071e2
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:17:07 +0300

    Update plans.controller.js

--- backend/src/controllers/plans.controller.js
+++ backend/src/controllers/plans.controller.js
@@ -1,16 +1,1 @@
-import { db } from "../config/firebase.js";
 
-export async function getPlans(req, res, next) {
-  try {
-    const snap = await db.collection("plans").where("active", "==", true).get();
-
-    const plans = snap.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-
-    res.json(plans);
-  } catch (err) {
-    next(err);
-  }
-}
