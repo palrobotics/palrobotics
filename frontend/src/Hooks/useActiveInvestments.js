ced4a4d9c4ee7d8f1c809877ea852add20ae239c
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:09:39 +0300

    Update useActiveInvestments.js

--- frontend/src/Hooks/useActiveInvestments.js
+++ frontend/src/Hooks/useActiveInvestments.js
@@ -1,17 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchActiveInvestments } from "../api/earnings";
 
-export function useActiveInvestments() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["active-investments"],
-    queryFn: async () => {
-      const res = await fetchActiveInvestments();
-      return res.data.investments;
-    },
-  });
-
-  return {
-    investments: data || [],
-    loading: isLoading,
-  };
-}
