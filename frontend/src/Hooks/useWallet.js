c15a931abd9cfdbd5d8823ca3d46fc552f63418c
palrobotics <palroboticsandinvestments@gmail.com>
2026-03-15 12:12:07 +0300

    Update useWallet.js

--- frontend/src/Hooks/useWallet.js
+++ frontend/src/Hooks/useWallet.js
@@ -1,18 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "../Context/AuthContext";
-import { fetchWallet } from "../Firebase/firestoreService";
 
-export function useWallet() {
-  const { user } = useAuth();
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["wallet", user?.uid],
-    queryFn: () => fetchWallet(user.uid),
-    enabled: !!user,
-  });
-
-  return {
-    wallet: data || null,
-    loading: isLoading,
-  };
-}
